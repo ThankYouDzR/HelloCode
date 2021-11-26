3b54d03316b122c8b7740e9bf8cb09eab901d6ec
ThankYouDzR <vkorn95@gmail.com>
2021-11-26 22:21:13 +0300

    added solution to task 2

---
+++ Tasks/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int second = Convert.ToInt32(Console.ReadLine());
+if(first > second)
+{
+    Console.WriteLine("Наибольшее число = " + first + " Наименьшее число = " + second);
+}
+else
+{
+    Console.WriteLine("Наибольшее число = " + second + " Наименьшее число = " + first);
+}
