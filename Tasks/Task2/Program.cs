ba727dea95e3b035fc02b0999f3fbe35d93df35c
ThankYouDzR <vkorn95@gmail.com>
2021-11-26 17:47:49 +0300

    solved the second task

--- Tasks/Task2/Program.cs
+++ Tasks/Task2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+string firstString = Console.ReadLine();
+int first = Convert.ToInt32(firstString);
+Console.WriteLine("Введите второе число");
+string secondString = Console.ReadLine();
+int second = Convert.ToInt32(secondString);
+int stepen = 2;
+if(first == Math.Pow(second,stepen))
+{    
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}    
