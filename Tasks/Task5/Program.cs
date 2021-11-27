59ea0a1095fe5597df27110b609beefb7eb9f220
ThankYouDzR <vkorn95@gmail.com>
2021-11-27 15:17:42 +0300

    added solution to task 5

---
+++ Tasks/Task5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+// График кубической функции (кубическая парабола): y = ax3 + bx2 + cx + d
+// y = 3x3 + 7x2 + 14x - 10
+
+Console.WriteLine("Введите значение x");
+int x = Convert.ToInt32(Console.ReadLine());
+double y = 3 * Math.Pow(x, 3) + 7 * Math.Pow(x, 2) + 14 * x - 10;
+Console.WriteLine("Значение функции: y = " + y);
