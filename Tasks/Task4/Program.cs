5b06df3c463d27de3676db9d2e7cebc0f9b5b9ad
ThankYouDzR <vkorn95@gmail.com>
2021-11-27 14:50:06 +0300

    added solution to task 4

---
+++ Tasks/Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти максимальное из трех чисел
+
+Console.WriteLine("Введите первое число");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int second = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int third = Convert.ToInt32(Console.ReadLine());
+int max = first;
+if(second > max) max = second;
+if(third > max) max = third;
+Console.WriteLine("Максимальное число = " + max);
