b941b9f73494c786ac366e89f52b719da6ebcdfa
ThankYouDzR <vkorn95@gmail.com>
2021-11-27 16:13:34 +0300

    added solution to task 7

---
+++ Tasks/Task7/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать числа от -N до N
+
+Console.WriteLine("Введите значение N");
+int N = Convert.ToInt32(Console.ReadLine());
+int result = -N;
+while(result<=N)
+{
+    Console.Write(result + " ");
+    result++;
+}
