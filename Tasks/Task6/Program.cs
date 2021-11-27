848f7d3f9dee78a99b96f84593702da5ae0d495f
ThankYouDzR <vkorn95@gmail.com>
2021-11-27 15:34:42 +0300

    added solution to task 6

---
+++ Tasks/Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Веедите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number%2==0) 
+{
+    Console.WriteLine("YES");
+}    
+else
+{
+    Console.WriteLine("NO");
+}    
