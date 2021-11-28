2f47a3732723cb2f327244902522a42267514b56
ThankYouDzR <vkorn95@gmail.com>
2021-11-28 19:58:52 +0300

    added solution to task 12

---
+++ Tasks/Task12/Program.cs
@@ -0,0 +1,14 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int first = number/100;
+int second = number%10;
+if(number <100 ^ number > 999)
+{
+    Console.WriteLine("Число не трёхзначное");
+}
+else
+{
+    Console.WriteLine(Convert.ToString(first)+Convert.ToString(second));
+}
