99b4d8fc938c9a92ea3119385893b96f0cd5d3af
ThankYouDzR <vkorn95@gmail.com>
2021-11-28 19:28:09 +0300

    added solution to task 8

---
+++ Tasks/Task8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Введите значение N");
+int N = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+while (result <= N)
+{
+    if (result%2==0)
+    {
+        Console.Write(result);
+    }
+    else
+    {
+        Console.Write(" ");
+    }
+    result++;
+}
