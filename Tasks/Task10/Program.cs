94677db98fbf59086bb8e501b5fb82fce29de567
ThankYouDzR <vkorn95@gmail.com>
2021-11-28 19:42:26 +0300

    added solution to task 10

---
+++ Tasks/Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number < 100 ^ number > 999)
+{
+    Console.WriteLine("Число не трёхзначное");
+}
+else
+{
+    Console.WriteLine(number / 10 % 10);
+}
