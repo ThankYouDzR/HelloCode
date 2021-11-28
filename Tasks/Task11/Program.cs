317e324221c7868613904b5ccee8bc74e7ba1d36
ThankYouDzR <vkorn95@gmail.com>
2021-11-28 19:49:15 +0300

    added solution to task 11

---
+++ Tasks/Task11/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int number = new Random().Next(10,100);
+Console.WriteLine("Случайное число: " + number);
+int first = number/10;
+int second = number%10;
+if(first>=second)
+{
+    Console.WriteLine("Наибольшая цифра числа: " + first);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра числа: " + second);
+}
