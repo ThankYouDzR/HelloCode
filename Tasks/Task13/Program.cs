40c359cbe080a275956c0c76ee39bf879a51a6da
ThankYouDzR <vkorn95@gmail.com>
2021-11-28 20:21:02 +0300

    added solution to task 13

---
+++ Tasks/Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int number1 = new Random().Next(1,1001);
+Console.WriteLine("Делимое число: "+ number1);
+Console.WriteLine("Введите число-делитель:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = number1 % number2;
+if((number1 % number2) == 0)
+{
+    Console.WriteLine("Число кратно заданному");
+}
+else
+{
+    Console.WriteLine("Остаток от деления = " + (number1%number2));
+}
