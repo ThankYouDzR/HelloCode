2967cdc65006b0ea02e31e19a473d996f3c5dbeb
ThankYouDzR <vkorn95@gmail.com>
2021-12-04 21:48:11 +0300

    added solution to task 20

---
+++ Tasks/Task20/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Please enter quarter number 1-4: "); 
+int num = int.Parse(Console.ReadLine());
+if(num == 1){
+    Console.WriteLine("X>0 Y>0");
+}
+if(num == 2){
+    Console.WriteLine("X<0 Y>0");
+}
+if(num == 3){
+    Console.WriteLine("X<0 Y<0");
+}
+if(num == 4){
+    Console.WriteLine("X>0 Y<0");
+}
