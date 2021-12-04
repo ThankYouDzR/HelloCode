d7f36748155308de95cbfeeda801f83a2ce509eb
ThankYouDzR <vkorn95@gmail.com>
2021-12-04 20:57:03 +0300

    added solution to task 15

---
+++ Tasks/Task15/Program.cs
@@ -0,0 +1,10 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+Console.WriteLine("Please enter number: "); 
+int Num = int.Parse(Console.ReadLine()); 
+if (Num % 7 == 0 || Num %23 == 0){
+    Console.WriteLine("Number is divisible by 7 or 23"); 
+}
+else{
+    Console.WriteLine("Number is not divisible by 7 or 23");
+}
