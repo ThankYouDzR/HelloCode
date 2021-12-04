d678c1de6db9ca5eea10aaa9fb6673a85957c1bd
ThankYouDzR <vkorn95@gmail.com>
2021-12-04 20:50:15 +0300

    added solution to task 14

---
+++ Tasks/Task14/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(number);
+if(number<100){
+    Console.WriteLine("Третьей цифры нет");
+}
+else{
+    while(number>1000){
+        number = number/10;
+    }
+    Console.WriteLine("Третья цифра в числе: " + (number%10));
+}
