1b4887141bb67cc9030bf68b0b24a28ac6558704
ThankYouDzR <vkorn95@gmail.com>
2021-12-04 21:18:52 +0300

    added solution to task 17

---
+++ Tasks/Task17/Program.cs
@@ -0,0 +1,12 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine("Please enter number: "); 
+int Num1 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Please enter number: "); 
+int Num2 = int.Parse(Console.ReadLine()); 
+if((Num1 == Math.Pow(Num2,2)) || (Num2 == Math.Pow(Num1,2)) ){
+    Console.WriteLine("YES!");
+}
+else{
+    Console.WriteLine("NO!");
+}
