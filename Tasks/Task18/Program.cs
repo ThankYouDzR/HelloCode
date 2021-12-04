4cb5b396121c586f59ddc9428fcaffca65e92efe
ThankYouDzR <vkorn95@gmail.com>
2021-12-04 21:31:47 +0300

    added solution to task 18

---
+++ Tasks/Task18/Program.cs
@@ -0,0 +1,12 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+Console.WriteLine("Please enter X true or false: "); 
+bool X = bool.Parse(Console.ReadLine()); 
+Console.WriteLine("Please enter Y true or false: "); 
+bool Y = bool.Parse(Console.ReadLine()); 
+if((!(X||Y)) == (!X && !Y)){
+    Console.WriteLine("All right!");
+}
+else{
+    Console.WriteLine("Error!");
+}
