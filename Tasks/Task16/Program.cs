ac3e423393a2c1e09c4fdaa2cca7a4bdf4564ede
ThankYouDzR <vkorn95@gmail.com>
2021-12-04 21:08:22 +0300

    added solution to task 16

---
+++ Tasks/Task16/Program.cs
@@ -0,0 +1,10 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным 
+
+Console.WriteLine("Please enter number 1-7: "); 
+int Num = int.Parse(Console.ReadLine()); 
+if (Num % 6 == 0 || Num %7 == 0){
+    Console.WriteLine("Yes, it's a day off!"); 
+}
+else{
+    Console.WriteLine("No, it's a weekday");
+}
