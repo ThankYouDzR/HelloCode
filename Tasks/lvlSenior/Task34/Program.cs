c3db7462288b8644d2afdc74d305b4f91150798b
ThankYouDzR <vkorn95@gmail.com>
2021-12-09 21:54:48 +0300

    added solution to task 34

---
+++ Tasks/lvlSenior/Task34/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу замену элементов массива на противоположные
+
+int [] array = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int n = array.Length;
+int sum1 = 0;
+int sum2 = 0;
+for(int i = 0; i < n; i++){
+    array[i] = new Random().Next(-100, 101);
+    Console.Write(array[i]+" ");
+}
+Console.WriteLine();
+for(int i = 0; i < n; i++){
+    array[i] = array[i] * (-1);
+    Console.Write(array[i]+" ");
+}    
