b2c17949e78c367f3be11199cf76583a96dfba46
ThankYouDzR <vkorn95@gmail.com>
2021-12-09 16:04:43 +0300

    added solution to task 23

---
+++ Tasks/lvlMiddle/Task23/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите значение N");
+int N = int.Parse(Console.ReadLine());
+for(int i = 1; i <= N; i++)
+{
+    Console.WriteLine(i + " ^ 2 = " + Math.Pow(i,2));
+}
