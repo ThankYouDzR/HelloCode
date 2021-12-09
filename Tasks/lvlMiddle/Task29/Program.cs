a4a709b26f2c6cfb361cf201a861cbf105d8913b
ThankYouDzR <vkorn95@gmail.com>
2021-12-09 18:06:26 +0300

    added solution to task 29

---
+++ Tasks/lvlMiddle/Task29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int result = 1;
+for(int i = 1; i <= N; i++)
+{
+    result = result * i;
+}
+Console.Write("Произведения чисел от 1 до " + N + " = " + result);
