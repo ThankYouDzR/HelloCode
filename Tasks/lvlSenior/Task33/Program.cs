e135e586b134b67af186805c523ec6772bf490bc
ThankYouDzR <vkorn95@gmail.com>
2021-12-09 21:50:13 +0300

    added solution to task 33

---
+++ Tasks/lvlSenior/Task33/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [-9,9]. Найти сумму положительных/отрицательных элементов массива
+
+int [] array = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int n = array.Length;
+int sum1 = 0;
+int sum2 = 0;
+for(int i = 0; i < n; i++){
+    array[i] = new Random().Next(-9, 10);
+    Console.Write(array[i]+" ");
+}
+Console.WriteLine();
+for(int i = 0; i < n; i++){
+    if(array[i] >= 0){
+        sum1 = sum1 + array[i];
+    }
+    else{
+        sum2 = sum2 + array[i];
+    }
+}
+Console.WriteLine("Сумма положительных элементов = " + sum1);
+Console.WriteLine("Сумма отрицательных элементов = " + sum2);
