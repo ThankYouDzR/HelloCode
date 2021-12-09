2539d52aa7aa68836f0d6caf7c624a9b0aaeafff
ThankYouDzR <vkorn95@gmail.com>
2021-12-09 17:55:06 +0300

    added solution to task 28

---
+++ Tasks/lvlMiddle/Task28/Program.cs
@@ -0,0 +1,12 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Write("Введите число: ");
+int numb = int.Parse(Console.ReadLine());
+int result = 0;
+int sum = 0;
+while(numb > 0){
+    result = numb % 10;
+    numb = numb / 10;
+    sum = sum + result;
+}
+Console.Write("Сумма цифр этом числе = " + sum);
