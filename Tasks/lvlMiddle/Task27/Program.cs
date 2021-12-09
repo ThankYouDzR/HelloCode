0b635fc140fb9b1546609718bd89a0828f793f1f
ThankYouDzR <vkorn95@gmail.com>
2021-12-09 17:37:21 +0300

    added solution to task 27

---
+++ Tasks/lvlMiddle/Task27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Определить количество цифр в числе
+
+Console.Write("Введите число: ");
+int numb = int.Parse(Console.ReadLine());
+int count = 0;
+while(numb>0){
+    numb = numb/10;
+    count++;
+}
+Console.Write("В этом числе " + count + " цифр");
+
+
