2d56d33649542fbe0862fceba2b195e13273b25e
ThankYouDzR <vkorn95@gmail.com>
2021-12-15 15:12:52 +0300

    added solution to task 36

---
+++ Tasks/lvlSenior/Task36/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+int [] array = new int[10];
+int n = array.Length;
+int count1 = 0;
+int count2 = 0;
+for(int i = 0; i < n; i++){
+    array[i] = new Random().Next(100, 1000);
+    Console.Write(array[i]+" ");
+}
+Console.WriteLine();
+for(int i = 0; i < n; i++){
+    if(array[i]%2==1){
+        count1++;
+    }
+    else{
+        count2++;
+    }    
+}
+Console.WriteLine($"Количество нечетных чисел: {count1}");
+Console.WriteLine($"Количество четных чисел: {count2}");
